e5f18b464468f6bd348dd197755049e2f95769b9
Minky Baek <55613446+toyo30@users.noreply.github.com>
2023-04-09 02:17:06 +0900

    fix: 줄간격 맞추기

--- project/app/views.py
+++ project/app/views.py
@@ -7,34 +7,30 @@ from .models import Comment, Post
 
 def signup(request):
    if request.method == 'POST':
-       username = request.POST['username']
-       password = request.POST['password']
-
-       exist_user = User.objects.filter(username=username)
-
-       if exist_user:
-            error = "이미 존재하는 유저입니다."
-            return render(request, 'registration/signup.html', {"error":error})
-       
-       new_user = User.objects.create_user(username=username, password=password)
-       auth.login(request, new_user)
+      username = request.POST['username']
+      password = request.POST['password']
+      exist_user = User.objects.filter(username=username)
+      if exist_user:
+           error = "이미 존재하는 유저입니다."
+           return render(request, 'registration/signup.html', {"error":error})
+      
+      new_user = User.objects.create_user(username=username, password=password)
+      auth.login(request, new_user)
    
-       return redirect('home')
+      return redirect('home')
        
    return render(request, 'registration/signup.html')
 
 def login(request):
    if request.method == 'POST':
-       username = request.POST['username']
-       password = request.POST['password']
-
-       user = auth.authenticate(username=username, password=password)
-
-       if user is not None:
-            auth.login(request, user)
-            return redirect('home')
-       error = "아이디 또는 비밀번호가 틀립니다"
-       return render(request, 'registration/login.html', {"error":error})
+      username = request.POST['username']
+      password = request.POST['password']
+      user = auth.authenticate(username=username, password=password)
+      if user is not None:
+           auth.login(request, user)
+           return redirect('home')
+      error = "아이디 또는 비밀번호가 틀립니다"
+      return render(request, 'registration/login.html', {"error":error})
         
    return render(request, 'registration/login.html')
 
